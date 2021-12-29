069ea0e57a4dd47d437e4ee55fd56a9b0a5b82df
kiranpe <kiranwls2011@gmail.com>
2021-12-29 22:26:28 +0530

    updated python code with f string in print

--- gcp-python-project-2/gcp_services_status.py
+++ gcp-python-project-2/gcp_services_status.py
@@ -47,16 +47,16 @@ def list_service_accounts(project_id):
 
   for account in service_accounts['accounts']:
      if account['displayName'] in 'readaccess':
-       console.print(":thumbs_up: Service Account | {} | [success]Exists![/success]\n".format(account['name']))
+       console.print(f":thumbs_up: Service Account | {account['name']} | [success]Exists![/success]\n")
 
 list_service_accounts(projectId)
 
 def check_bucket():
     bucket = storage_client.get_bucket(bucket_name)
     if bucket.name in bucket_name:
-      console.print(":thumbs_up: Bucket | {} | [success]Exists![/success]\n".format(bucket.name)) 
+      console.print(f":thumbs_up: Bucket | {bucket.name} | [success]Exists![/success]\n") 
     else:
-      print(":thumbs_down: Bucket | {} | [error]Doesn't Exists![/error]\n".format(bucket.name))
+      console.print(":thumbs_down: Bucket | {bucket.name} | [error]Doesn't Exists![/error]\n")
 
 check_bucket()
 
@@ -70,11 +70,11 @@ def upload_file():
   try:
     object_name_in_gcs_bucket = bucket.blob(destination_blob_name)
     object_name_in_gcs_bucket.upload_from_filename(source_file_name)
-    console.print(":thumbs_up: file: {} uploaded to bucket: {} | [success]Successful[/success]\n".format(source_file_name,bucket.name))
+    console.print(f":thumbs_up: file: {source_file_name} uploaded to bucket: {bucket.name} | [success]Successful[/success]\n")
   except Exception as e:
-    console.print(":thumbs_down: file: {} upload to bucket: {} | Failed".format(source_file_name,bucket.name), style="error")
+    console.print(f":thumbs_down: file: {source_file_name} upload to bucket: {bucket.name} | Failed")
     if e.code == 403:
-      console.print(" ⚠️  Service Account {} does not have storage.objects.create access to the 🪣  bucket: {}\n".format(sa,bucket.name))
+      console.print(f"  ⚠️  Service Account: {sa} does not have storage.objects.create access to the 🪣  bucket: {bucket.name}\n")
  
 upload_file()
 
@@ -82,18 +82,18 @@ def instance_status():
   for instance_name in instances:
     response = service.instances().get(project=projectId, zone=zone, instance=instance_name).execute()
     if response['status'] in 'RUNNING':
-     console.print(":thumbs_up: {} | status | [success]{}[/success]\n".format(instance_name,response['status']))
+     console.print(f":thumbs_up: {instance_name} | status | [success]{response['status']}[/success]\n")
     else:
-     console.print(":thumbs_down: {} | status | [error]{}[/error]\n".format(instance_name,response['status']))
+     console.print(f":thumbs_down: {instance_name} | status | [error]{response['status']}[/error]\n")
    
 instance_status()
 
 def ping_instance():
   try:
     subprocess.call(['bash', './ping_test.sh', instances[0], instances[1], zone])
-    print(" ")
+    print()
   except:
-    print(" ")
+    print()
 
 ping_instance()
 
@@ -101,9 +101,9 @@ def check_docker_service():
  for vm in instances:
   try:
    subprocess.call(['bash', './check_docker_service.sh', projectId, zone, vm])
-   print(" ")
+   print()
   except:
-    print("Script is Failed!! Check script once!!\n")
+    console.print(f"[error]Script is Failed!! Check script once!![/error]\n")
  
 check_docker_service()
 
@@ -115,24 +115,24 @@ def container_status():
      response = service.instances().get(project=projectId, zone=zone, instance=instance_name).execute()
      ips.append(response['networkInterfaces'][0]['accessConfigs'][0]['natIP'])
  except:
-   console.print(":thumbs_down: [error]{} Server is NOT RUNNING!! Not able to get IP Address!![/error]\n".format(instance_name))
+   console.print(f":thumbs_down: [error]{instance_name} Server is NOT RUNNING!! Not able to get IP Address!![/error]\n")
 
  for host in ips:
   container_urls = ["http://" +host+ ":10800" +"/index.html", "http://" +host+ ":10801" +"/index.html"]
-  console.print("[status]Checking Container Status on {}!![/status]".format(host))
+  console.print(f"[status]Checking Container Status on {host}!![/status]")
   for url in container_urls:
    try:
      response = requests.get(url)
      if response.status_code == 200:
        if urlparse(url).port == 10800:
-         console.print("✅ [success]Nginx Container is Up and Running on host:[/success] {}\n".format(host))
+         console.print(f"✅ [success]Nginx Container is Up and Running on host:[/success] {host}\n")
        else:
-         console.print("✅ [success]Apache Container is Up and Running on host:[/success] {}\n".format(host))
+         console.print(f"✅ [success]Apache Container is Up and Running on host:[/success] {host}\n")
    except:
      if urlparse(url).port == 10800:
-       console.print("❌ [error]Nginx Container on host:[/error] {} [error]is not running. Please check.[/error]\n".format(host))
+       console.print(f"❌ [error]Nginx Container on host:[/error] {host} [error]is not running. Please check.[/error]\n")
      else:
-       console.print("❌ [error]Apache Container on host:[/error] {} [error]is not running. Please check.[/error]\n".format(host))
+       console.print(f"❌ [error]Apache Container on host:[/error] {host} [error]is not running. Please check.[/error]\n")
 
 container_status()
 
